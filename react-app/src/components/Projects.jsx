8470bd774160fa5e4253b9eee7cc77d6e2b75449
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-01 17:30:57 +0000

    draft Projects Page

--- react-app/src/components/Projects.jsx
+++ react-app/src/components/Projects.jsx
@@ -1,40 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-import logo from "../covercrop.jpg";
 import "../App.css";
 
+// import projectData from './projectData.json';
+// import Project from './Project.js';
+// import ProjectNav from './ProjectNav.js';
+
 const Projects = () => {
+
+const [projects, setProjects] = useState([]);
+const { projectId } = useParams();
+  
+useEffect(() => {
+  setProjects(projectData);
+}, []);
+
   return (
-    <div className="App-home">
-      <p>
-        <code>
-          <span className="transparent">Frontend Developer specializing in</span>
-          <br />
-          PROJECTS
-          <br />
-          <span className="transparent">interfaces to APIs</span>
-        </code>
-      </p>
-      <a
-        className='App-link'
-        href="https://github.com/stevelab1"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <p>
-          <code className='App-link'>stevelab1</code>
-        </p>
-        {/* <br /> */}
-        <p>
-          <img
-            style={{ borderRadius: "50%" }}
-            src={logo}
-            className="App-logo-small"
-            alt="logo"
-          />
-        </p>
-      </a>
-    </div>
+    <main>
+    <h1>Projects</h1>
+    <CardDeck className="blue-card-deck">
+        {projects.map((project) => (
+            <Project 
+                key={project.id}
+                title={project.title}
+                deployedLink={project.deployedLink}
+                githubLink={project.githubLink}
+                screenshot={project.screenshot} 
+            />
+        ))}
+    </CardDeck>
+    <ProjectNav projects={projects} projectId={projectId} />
+</main>
   );
 };
 
