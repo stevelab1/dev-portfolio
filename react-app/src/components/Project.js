58c4b82f8ecc0de602d075a4bb44c29210383aa9
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-01 17:49:54 +0000

    correcting Project Card component

--- react-app/src/components/Project.js
+++ react-app/src/components/Project.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardLink } from 'react-bootstrap';
+import { Card, CardImg } from 'react-bootstrap';
 
 const Project = ({ title, deployedLink, githubLink, screenshot }) => {
     return (
         <Card className="orange-card">
             <CardImg src={screenshot} />
-            <CardBody>
-                <CardTitle>{title}</CardTitle>
-                <CardSubtitle>
-                    <CardLink href={deployedLink}>Deployed Version</CardLink>
-                    <CardLink href={githubLink}>GitHub Repository</CardLink>
-                </CardSubtitle>
-            </CardBody>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Subtitle>
+                    <Card.Link href={deployedLink}>Deployed Version</Card.Link>
+                    <Card.Link href={githubLink}>GitHub Repository</Card.Link>
+                </Card.Subtitle>
+            </Card.Body>
         </Card>
     );
 }
