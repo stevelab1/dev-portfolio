5e2bc1717f2c69eee69da56d0ddedba2805ddd2b
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-04 14:31:57 +0000

    Contact page

--- react-app/src/pages/Contact.jsx
+++ react-app/src/pages/Contact.jsx
@@ -18,57 +18,84 @@ const Contact = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(formData);
-    // Add additional code here to submit the form data to a server
+    // Add additional code here if you want to submit the form data to a server
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Form
-        className="text-white p-4"
-        onSubmit={handleSubmit}
-        data-netlify="true"
-        name="contact"
-      >
-        <Form.Group>
-          <Form.Label>Your Name:</Form.Label>
-          <p>
-            <Form.Control
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </p>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Your Email:</Form.Label>
-          <p>
-            <Form.Control
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </p>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Message:</Form.Label>
-          <p>
-            <Form.Control
-              as="textarea"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-          </p>
-        </Form.Group>
+    <>
+      <br />
+      <div className="text-white p-4">
+        <p>Find me on: </p>
         <p>
-          <Button variant="primary" type="submit">
-            Send
-          </Button>
+          <a
+            href="https://github.com/stevelab1"
+            target="_blank"
+            rel="noopener noreferrer"
+            alt="link to stevelab1 github profile"
+          >
+            GitHub
+          </a>
         </p>
-      </Form>
-    </div>
+        <p>
+          <a
+            href="https://www.linkedin.com/in/stephen-t-823341193"
+            target="_blank"
+            rel="noopener noreferrer"
+            alt="link to stevelab1 LinkedIn profile"
+          >
+            LinkedIn
+          </a>
+        </p>
+        ... or contact me via email using the form below:
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Form
+          className="text-white p-4"
+          onSubmit={handleSubmit}
+          data-netlify="true"
+          name="contact"
+        >
+          <Form.Group>
+            <Form.Label>Your Name:</Form.Label>
+            <p>
+              <Form.Control
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </p>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Your Email:</Form.Label>
+            <p>
+              <Form.Control
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </p>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Message:</Form.Label>
+            <p>
+              <Form.Control
+                as="textarea"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              />
+            </p>
+          </Form.Group>
+          <p>
+            <Button variant="primary" type="submit">
+              Send
+            </Button>
+          </p>
+        </Form>
+      </div>
+    </>
   );
 };
 
