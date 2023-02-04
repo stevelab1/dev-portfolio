2d6c5e1805dd5a115669f97b50cdc57203378c2d
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-04 02:05:47 +0000

    stateful Contact component

--- react-app/src/pages/Contact.jsx
+++ react-app/src/pages/Contact.jsx
@@ -1,44 +1,63 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    // You could add additional code here to submit the form data to a server
+  };
+
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Form
-        // style={{  }}
-        className="text-white p-4"
-        name="contact"
-        method="POST"
-        data-netlify="true"
-      >
-        <Form.Group>
-          <Form.Label>Your Name:</Form.Label>
-          <p>
-            {" "}
-            <Form.Control type="text" name="name" />{" "}
-          </p>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Your Email:</Form.Label>
-          <p>
-            <Form.Control type="email" name="email" />
-          </p>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Message:</Form.Label>
-          <p>
-            {" "}
-            <Form.Control as="textarea" name="message" />
-          </p>
-        </Form.Group>
-        <p>
-          {" "}
-          <Button variant="primary" type="submit">
-            Send
-          </Button>
-        </p>
-      </Form>
-    </div>
+    <form onSubmit={handleSubmit} data-netlify="true" name="contact">
+      <p>
+        <label>
+          Your Name:{" "}
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:{" "}
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:{" "}
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 };
 
